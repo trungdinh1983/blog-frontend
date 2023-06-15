ba207e097f7084627f10dcddae3a1ff8da5cd024
YOUR NAMETrung <trungdinh1983@gmail.com>
2023-06-15 12:21:14 -0700

    Use state

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+import axios from "axios";
+import { useState } from "react";
+
 function Header() {
   return (
     <header>
@@ -23,78 +26,40 @@ function PostsNew() {
   );
 }
 
-// function PostsIndex(props) {
-//   return (
-//     <div id="posts-index">
-//       <h1> All Posts</h1>
-//       {props.posts.map((post) => (
-//         <div className="posts" key={post.id}>
-//           <h2>{post.title}</h2>
-//           <img src={post.image_url} alt="" />
-//           <p>Authour: {post.author}</p>
-//           <button>More Info</button>
-//         </div>
-//       ))}
-//     </div>
-//   );
-// }
-
-function PostsIndex() {
+function PostsIndex(props) {
   return (
     <div id="posts-index">
-      <h1>All posts</h1>
-      <div className="posts">
-        <h2>Burger is amazing</h2>
-        <img
-          src="https://lh3.googleusercontent.com/p/AF1QipP7HmmF0VuGmHJECHU4nKiHVTtbhOjIF2t3jSGV=s1360-w1360-h1020"
-          alt=""
-        />
-        <p>By: Jane Doe</p>
-        <button>More info</button>
-      </div>
-      <div className="posts">
-        <h2>Barbeque is great</h2>
-        <img
-          src="https://lh5.googleusercontent.com/p/AF1QipMfOZTsUjwouGtt9oXUpvBY2CSb9YkVFwCS_waX=w520-h350-n-k-no"
-          alt=""
-        />
-        <p>By: John Doe</p>
-        <button>More info</button>
-      </div>
+      <h1> All Posts</h1>
+      {props.posts.map((post) => (
+        <div className="posts" key={post.id}>
+          <h2>{post.title}</h2>
+          <img src={post.image_url} alt="" />
+          <p>Authour: {post.author}</p>
+          <button>More Info</button>
+        </div>
+      ))}
     </div>
   );
 }
 
-// function Content() {
-//   let posts = [
-//     {
-//       id: 1,
-//       title: "Burger",
-//       author: "Jane Doe",
-//       image_url: "https://lh3.googleusercontent.com/p/AF1QipP7HmmF0VuGmHJECHU4nKiHVTtbhOjIF2t3jSGV=s1360-w1360-h1020",
-//     },
-//     {
-//       id: 2,
-//       title: "BBQ",
-//       author: "Jone Doe",
-//       image_url: "https://lh5.googleusercontent.com/p/AF1QipMfOZTsUjwouGtt9oXUpvBY2CSb9YkVFwCS_waX=w520-h350-n-k-no",
-//     },
-//     {
-//       id: 3,
-//       title: "Mac and Cheese",
-//       author: "Jimmy Johnson",
-//       image_url:
-//         "https://www.thechunkychef.com/wp-content/uploads/2018/02/Ultimate-Creamy-Baked-Mac-and-Cheese-baking-dish.jpg",
-//     },
-//   ];
+function Content() {
+  const [posts, setPosts] = useState([]);
 
-//   return (
-//     <div>
-//       <PostsNew />
-//       <PostsIndex posts={posts} />
-//     </div>
-//   );
-// }
+  const handleIndexPosts = () => {
+    axios.get("http://localhost:5173/Post.json").then((response) => {
+      console.log(response.data);
+      posts = response.data;
+    });
+  };
+
+  return (
+    <div>
+      <PostsNew />
+      <button onClick={handleIndexPosts}>Load Posts</button>
+      <PostsIndex posts={posts} />
+    </div>
+  );
+}
 
 function Footer() {
   return (
@@ -108,8 +73,7 @@ function App() {
   return (
     <div>
       <Header />
-      <PostsNew />
-      <PostsIndex />
+      <Content />
       <Footer />
     </div>
   );
